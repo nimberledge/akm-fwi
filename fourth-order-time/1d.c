e5897f76d11047a6bb9ee1bb43bdc0d305cf207a
akhil <akhil2@illinois.edu>
2022-06-28 14:00:08 +0100

    update .c file for thoroughness

--- fourth-order-time/1d.c
+++ fourth-order-time/1d.c
@@ -94,3 +94,18 @@ u[t2][x + 2] = ((r3*r3)*(5.0e-1F*((5.0e-1F*b[x] - 2.0F*b[x + 1] + 1.5F*b[x + 2])
                 + (-1.5F*b[x + 2] + 2.0F*b[x + 3] - 5.0e-1F*b[x + 4])*(-1.5F*u[t0][x + 2] + 2.0F*u[t0][x + 3] - 5.0e-1F*u[t0][x + 4])))
                 + r4*damp[x + 1]*u[t0][x + 2] + r5*(-r2*(-2.0F*u[t0][x + 2]) - r2*u[t1][x + 2]))
                 /(r2*r5 + r4*damp[x + 1]);
+
+
+// pde = model.m * u.dt2 - 1/model.b * (model.b * u.dxr).dxl
+u[t2][x + 2] = r4*(dt*dt)*(((r2*r2)*(5.0e-1F*(-1.5F*u[t0][x] + 2.0F*u[t0][x + 1] - 5.0e-1F*u[t0][x + 2])*b[x] - 2.0F*(-1.5F*u[t0][x + 1] + 
+				2.0F*u[t0][x + 2] - 5.0e-1F*u[t0][x + 3])*b[x + 1] + 1.5F*(-1.5F*u[t0][x + 2] + 2.0F*u[t0][x + 3] - 5.0e-1F*u[t0][x + 4])*b[x + 2]))/b[x + 2]
+		 		+ (-r3*(-2.0F*u[t0][x + 2]) - r3*u[t1][x + 2])/r4);
+
+// pde = model.m * u.dt2 - 1/model.b * (model.b * u.dx2 + model.b.dxc * u.dxc)
+u[t2][x + 2] = r5*(dt*dt)*((2.5e-1F*(r2*r2)*(-b[x + 1] + b[x + 3])*(-u[t0][x + 1] + u[t0][x + 3]) + 
+				(r3*r6 + r3*u[t0][x + 1] + r3*u[t0][x + 3])*b[x + 2])/b[x + 2]
+			    + (-r4*r6 - r4*u[t1][x + 2])/r5);
+
+// pde = model.m * u.dt2 - 1/model.b * (model.b * u.dxc).dxc
+u[t2][x + 2] = r4*(dt*dt)*((2.5e-1F*(-r5*(-u[t0][x] + u[t0][x + 2])*b[x + 1] + r5*(-u[t0][x + 2] + u[t0][x + 4])*b[x + 3]))/b[x + 2]
+			   + (-r3*(-2.0F*u[t0][x + 2]) - r3*u[t1][x + 2])/r4);
